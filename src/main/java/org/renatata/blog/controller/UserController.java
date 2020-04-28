221269d887886446683b537a08d8eca9e09517f2
Renata Abreu <renata89abreu@gmail.com>
2020-04-28 17:52:59 -0300

    added user registration and some others UserService methods

--- src/main/java/org/renatata/blog/controller/UserController.java
+++ src/main/java/org/renatata/blog/controller/UserController.java
@@ -1,9 +1,36 @@
 package org.renatata.blog.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.json.JSONException;
+import org.renatata.blog.entity.User;
+import org.renatata.blog.model.UserResponse;
+import org.renatata.blog.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/user")
 public class UserController {
+
+    @Autowired
+    private UserService userService;
+
+    @PostMapping("/registration")
+    @ResponseBody
+    public ResponseEntity<UserResponse> add(@RequestBody User user) throws JSONException {
+        try {
+            Optional<User> userExists = userService.findUserByEmail(user.getEmail());
+            if (userExists.isPresent()) {
+                return new ResponseEntity<UserResponse>(new UserResponse(false, "User already exists.", userExists.get().getEmail()), HttpStatus.BAD_REQUEST);
+            }
+
+            userService.add(user);
+            return new ResponseEntity<>(new UserResponse(true, "User created.", user.getEmail()), HttpStatus.CREATED);
+        } catch (Exception e) {
+            return new ResponseEntity<>(new UserResponse(false, e.toString(), user.getEmail()), HttpStatus.BAD_REQUEST);
+        }
+    }
 }
--- src/main/java/org/renatata/blog/service/UserService.java
+++ src/main/java/org/renatata/blog/service/UserService.java
@@ -3,6 +3,7 @@ package org.renatata.blog.service;
 import org.renatata.blog.entity.User;
 import org.renatata.blog.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import java.util.Optional;
@@ -12,7 +13,30 @@ public class UserService {
     @Autowired
     private UserRepository userRepository;
 
+    @Autowired
+    private BCryptPasswordEncoder bCryptPasswordEncoder;
+
     public Optional<User> findUserByEmail(String email) {
         return userRepository.findUserByEmail(email);
+    }
+
+    public User add(User user) {
+        user.setPassword(bCryptPasswordEncoder.encode(user.getPassword()));
+        user.setActive(true);
+        return userRepository.save(user);
+    }
+
+    public void delete(Integer id) {
+        userRepository.deleteById(id);
+    }
+
+    public User changePassword(Integer userId, String oldPassword, String newPassword) {
+        Optional<User> user = userRepository.findById(userId);
+        if(user.isPresent() && bCryptPasswordEncoder.encode(oldPassword) == user.get().getPassword()) {
+            user.get().setPassword(bCryptPasswordEncoder.encode(newPassword));
+            return userRepository.save(user.get());
+        }
+
+        return null;
     }
 }
