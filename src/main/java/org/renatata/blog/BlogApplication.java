6061f52935989633d1053c6eeaa5ae6e59506e1a
Renata Abreu <renata89abreu@gmail.com>
2020-09-27 15:29:43 -0300

    addCorsMappings

--- src/main/java/org/renatata/blog/BlogApplication.java
+++ src/main/java/org/renatata/blog/BlogApplication.java
@@ -2,6 +2,9 @@ package org.renatata.blog;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class BlogApplication {
@@ -10,4 +13,17 @@ public class BlogApplication {
 		SpringApplication.run(BlogApplication.class, args);
 	}
 
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/*")
+						.allowedOrigins("*")
+						.allowedMethods("GET", "POST", "OPTIONS", "PUT")
+						.allowedHeaders("Content-Type", "X-Requested-With", "accept", "Origin", "Access-Control-Request-Method", "Access-Control-Request-Headers")
+						.exposedHeaders("Access-Control-Allow-Origin", "Access-Control-Allow-Credentials")
+						.allowCredentials(true).maxAge(3600);			}
+		};
+	}
 }
