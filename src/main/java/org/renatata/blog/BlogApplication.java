f9157336df77c0d869af6ba34aad2d1ee533c235
Renata Abreu <renata89abreu@gmail.com>
2020-09-28 16:11:27 -0300

    fixed Cors mapping

--- src/main/java/org/renatata/blog/BlogApplication.java
+++ src/main/java/org/renatata/blog/BlogApplication.java
@@ -18,7 +18,7 @@ public class BlogApplication {
 		return new WebMvcConfigurer() {
 			@Override
 			public void addCorsMappings(CorsRegistry registry) {
-				registry.addMapping("/*")
+				registry.addMapping("/**")
 						.allowedOrigins("*")
 						.allowedMethods("GET", "POST", "OPTIONS", "PUT")
 						.allowedHeaders("Content-Type", "X-Requested-With", "accept", "Origin", "Access-Control-Request-Method", "Access-Control-Request-Headers")
